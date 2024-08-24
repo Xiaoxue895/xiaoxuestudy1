8f97b6d130fa0c7f157014a30897e07eb6bedb33
Xiaoxue <xiaoxuew895@gmail.com>
2024-08-23 21:52:42 -0700

    w8d5

--- module-3/w8/d3/guided-practices/05-fetch-basic/fetch-requests.js
+++ module-3/w8/d3/guided-practices/05-fetch-basic/fetch-requests.js
@@ -35,3 +35,16 @@ fetch('/products')
 fetch('/products')
   .then((res) => res.text()) 
   .then((body) => console.log(body));
+
+
+// fetch basic 
+
+// fetch('https://api.example.com/submit', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ name: 'John', age: 30 })
+//   })
+//     .then(response => response.json()) // 解析 JSON
+//     .then(data => console.log(data))    // 打印数据
+//     .catch(error => console.error('Error:', error)); // 处理错误
+  
