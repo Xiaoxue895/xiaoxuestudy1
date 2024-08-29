fa3bf1825160dd85f1450e38745b888fbf341b48
Xiaoxue <xiaoxuew895@gmail.com>
2024-08-28 20:16:12 -0700

    w9d3

--- module-3/w9/d3/guided-practices/02-data-on-elements/data.js
+++ module-3/w9/d3/guided-practices/02-data-on-elements/data.js
@@ -1,1 +1,23 @@
 // Your code here 
+function addShoppingItem(event){
+    event.preventDefault();
+
+    const shoppinglist = document.getElementById("shopping-list");
+    const name = document.getElementById("name");
+    const type = document.getElementById('type');
+
+    const li = document.createElement('li');
+
+    li.innerText = name.value;
+    li.dataset.type = type.value;
+
+    shoppinglist.appendChild(li);
+
+    name.value = "";
+    type.value = "dairy";
+
+}
+
+window.addEventListener('DOMContentLoaded',(event)=>{
+    document.getElementById("add").addEventListener("click",addShoppingItem)
+});
