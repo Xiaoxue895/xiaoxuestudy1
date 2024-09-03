1bcc8c77c594c63ba137beb3edf9f045b2cd84e5
Xiaoxue <xiaoxuew895@gmail.com>
2024-09-03 10:19:05 -0700

    w10d2

---
+++ Module-4/W10/D1/4-express-route-order/server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Server is alive");
+});
+
+app.get('/hello', (req, res) => {
+    res.send("Hello, my friend!");
+});
+
+app.get('/goodbye', (req, res) => {
+    res.send("Goodbye, my friend!");
+});
+
+app.get('/goodbye/until/tomorrow', (req, res) => {
+    res.send('Have a nice day! See you tomorrow.');
+});
+
+app.get('/goodbye/until/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
