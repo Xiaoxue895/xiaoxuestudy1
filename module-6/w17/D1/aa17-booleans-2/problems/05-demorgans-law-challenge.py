b858e0d092d501c11cd7ee66d772c49a704e70a8
Xiaoxue <xiaoxuew895@gmail.com>
2024-12-03 10:07:01 -0800

    w17d2

---
+++ module-6/w17/D1/aa17-booleans-2/problems/05-demorgans-law-challenge.py
@@ -0,0 +1,20 @@
+# Create a function that uses DeMorgan's Law to get the expected `True`/`False`
+# statement.
+
+# Write your function here.
+
+# DeMorgan's Law: not (A and B) = (not A) or (not B)
+
+
+def de_morgans_law(arg1, arg2):
+    
+    return not (arg1 and arg2)
+
+
+
+print(de_morgans_law(True, True)) # False
+print(de_morgans_law(True, False)) # True
+print(de_morgans_law(False, False)) # True
+print(de_morgans_law("", [])) # True
+print(de_morgans_law(2, 2)) # False
+print(de_morgans_law(2, 0)) # True
